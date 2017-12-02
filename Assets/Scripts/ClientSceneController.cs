fb87c22693191a740d72d9c3abd7bedc615102dd
Christopher Forseth <christopher.forseth@gmail.com>
2017-12-02 11:51:25 -0600

    Switch an image over to a sprite. CleintController startup. Fix a network compile bug.

---
+++ Assets/Scripts/ClientSceneController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class ClientSceneController : MonoBehaviour
+{
+   enum eClientState
+   {
+      DISCOVER, 
+      NOT_READY,
+      READY, 
+      IN_GAME,  
+   }
+
+   public GameObject Title;
+   public GameObject ReadyUpUI;
+   public GameObject WolfUI; 
+   public GameObject HopperUI; 
+}
+
