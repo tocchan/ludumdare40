070ed1f3a606b190162d8eb2c84c68d246a13213
Christopher Forseth <christopher.forseth@gmail.com>
2017-12-02 13:21:37 -0600

    Client Controller State.

---
+++ Assets/Scripts/Network/UI/ClientConnectionStatus.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClientConnectionStatus : MonoBehaviour
+{
+   public Text StatusText; 
+
+   // Use this for initialization
+   void Start()
+   {
+   }
+   
+   // Update is called once per frame
+   void Update()
+   {
+      HopperNetwork.eState state = HopperNetwork.GetState();
+      switch (state) {
+         case HopperNetwork.eState.DISCONNECTED: 
+            StatusText.text = "Disconnected";
+            break;
+
+         case HopperNetwork.eState.CLIENT_LOOKING:
+            StatusText.text = "SEARCHING..."; 
+            break;
+
+         case HopperNetwork.eState.CLIENT_READY: 
+            StatusText.text = "READY";
+            break;
+
+         default:
+            StatusText.text = "UNKNOWN";
+            break;
+      }
+   }
+}
+
--- Assets/Scripts/ServerController.cs
+++ Assets/Scripts/ServerController.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking; 
 
 public class ServerController : MonoBehaviour
 {
-   // Use this for initialization
    void Start()
    {
-      
    }
    
    // Update is called once per frame
