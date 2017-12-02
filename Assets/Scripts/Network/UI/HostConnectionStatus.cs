3c4ed6e7138ee698037c5143611e1f09bc9ce0b2
Christopher Forseth <christopher.forseth@gmail.com>
2017-12-02 13:50:54 -0600

    Game now hosts.

--- Assets/Scripts/Network/UI/HostConnectionStatus.cs
+++ Assets/Scripts/Network/UI/HostConnectionStatus.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class HostConnectionStatus : MonoBehaviour
 {
-   public Text AddressText; 
-   public Text ConnectionCountText; 
+   public Text AddressText;
+   public Text ConnectionCountText;
 
    // Use this for initialization
    void Start()
    {
       AddressText.text = "Hosting: " + Network.player.ipAddress;
-      ConnectionCountText.text = "Players: " + HopperNetwork.GetConnectionCount();  
+      ConnectionCountText.text = "Players: " + HopperNetwork.GetConnectionCount();
+
+      HopperNetwork.Instance.OnPlayerJoin += OnConnectionChanged;
+      HopperNetwork.Instance.OnPlayerLeave += OnConnectionChanged;
+   }
+
+   void OnConnectionChanged( NetworkConnection conn )
+   {
+      ConnectionCountText.text = "Players: " + HopperNetwork.GetConnectionCount();
    }
 }
 
