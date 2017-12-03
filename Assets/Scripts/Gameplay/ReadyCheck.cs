9499eef21f53d5542b27cb1b006dd6cb9535a816
CLAYHOWELL\Clay <clayh7@gmail.com>
2017-12-03 13:31:12 -0600

    Ready check added

---
+++ Assets/Scripts/Gameplay/ReadyCheck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//-------------------------------------------------------------------------------------------------
+public class ReadyCheck : MonoBehaviour
+{
+	public PreyController m_controllerReference;
+	private SpriteRenderer m_renderer;
+
+
+	//-------------------------------------------------------------------------------------------------
+	private void Start()
+	{
+		m_renderer = GetComponent<SpriteRenderer>();
+	}
+
+
+	//-------------------------------------------------------------------------------------------------
+	private void Update()
+	{
+		if(m_controllerReference == null)
+		{
+			return;
+		}
+
+		//Visible if ready / Hidden if not ready
+		bool isVisible = false;
+		if(GameManager.GetInstance().m_currentState == eGameState.WAIT_FOR_READY)
+		{
+			if(m_controllerReference.m_netController != null)
+			{
+				isVisible = m_controllerReference.m_netController.ClientIsReady;
+			}
+		}
+		m_renderer.enabled = isVisible;
+	}
+}
