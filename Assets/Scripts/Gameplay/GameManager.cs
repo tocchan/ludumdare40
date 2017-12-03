62f80a349ac91eeed18f1cda178b2ffae698976f
Christopher Forseth <christopher.forseth@gmail.com>
2017-12-03 16:19:51 -0600

    Last 10 seconds is intense mode.

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -379,7 +379,7 @@ public class GameManager : MonoSingleton<GameManager>
 			aliveIntensity *= (1.0f - m_targetPitch); // this is 1 when we're fattest, so this speed up only applies if the fox is "fast"
 
          float remaining = m_gameDuration - m_gameTimer; 
-         if (remaining < 30.0f) {
+         if (remaining < 10.0f) {
             timeIntensity = .5f;
          }
 		}
